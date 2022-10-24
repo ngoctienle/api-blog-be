137f76cd8f10ccdc246e01f51ff24164b46fe9be
ngoctienle <ngoctien@bincg.com>
2022-10-24 11:22:51 +0700

    Admin Update

---
+++ wp-content/plugins/adminify/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f7015b582ac5e6604e45059977e2218afc6cc57f',
+        'name' => 'litonarefin/wp-adminify',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'litonarefin/wp-adminify' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f7015b582ac5e6604e45059977e2218afc6cc57f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
